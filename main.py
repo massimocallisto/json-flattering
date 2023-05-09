5c7db91f2ff5e976a9b32e14e6d890e862cf29f0
Matteo Romagnoli <matteo02.romagnoli@studenti.unicam.it>
2023-05-09 09:20:18 +0200

    Updated code using thingsboard-mqtt-library
    Imported libraries psutil and tb_gateway_mqtt;
    Added send gateways telemetries' method (cpu and memory %);
    Extended flatter_json method for "meta" field.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import time
+import psutil
 
 import paho.mqtt.client as mqtt
 import logging
@@ -7,23 +8,23 @@ import traceback
 import os
 import sys
 from datetime import datetime
+from tb_gateway_mqtt import TBGatewayMqttClient
 
 MQTT_IN = {
-    'host' : os.getenv('MQTT_IN_BROKER', "localhost"),
+    'host': os.getenv('MQTT_IN_BROKER', "localhost"),
     'port': int(os.getenv('MQTT_IN_PORT', "1883")),
     'username': os.getenv('MQTT_IN_USERNAME', ""),
     'password': os.getenv('MQTT_IN_PASSWORD', ""),
     'source_topic': os.getenv('MQTT_IN_TOPIC', "/in/#"),
-    'client' : None
+    'client': None
 }
 
 MQTT_OUT = {
-    'host' : os.getenv('MQTT_OUT_BROKER', None),
+    'host': os.getenv('MQTT_OUT_BROKER', None),
     'port': int(os.getenv('MQTT_OUT_PORT', "1883")),
     'username': os.getenv('MQTT_OUT_USERNAME', ""),
     'password': os.getenv('MQTT_OUT_PASSWORD', ""),
-    'out_topic': os.getenv('MQTT_OUT_TOPIC_PREFIX', "/flat"),
-    'client' : None
+    'gateway': None
 }
 
 logger = logging.getLogger("mqtt_connector")
@@ -39,33 +40,24 @@ def on_connect_out_2(client, userdata, flags, rc):
 
 
 def on_message_in(client, userdata, msg):
+    if MQTT_OUT["gateway"] is None:
+        return
+
     try:
         text = msg.payload.decode('utf-8')
         logger.info(f"got new msg on topic {msg.topic}\nText: {text}")
         json_text = flatter_json(text)
+        json_str = json.dumps(json_text)
+        json_obj = json.loads(json_str)
+        json_to_send = json_obj
+        device_ref = json_obj['ref']
+        device_type = json_obj['type']
+        device_id = device_type + "_" + device_ref
 
-        if MQTT_OUT["client"] is not None:
-            curr_client = MQTT_OUT["client"]
-        else:
-            if MQTT_IN["source_topic"] == "#":
-                logger.warning("You cannot publish on the same broker by using # subscription otherwise "
-                               "a loop will happen!")
-                return
-            if msg.topic.startswith(MQTT_OUT["out_topic"]):
-                logger.warning(f"Loop detected! Input topic {msg.topic} start with out topic"
-                               + MQTT_OUT["out_topic"] + "!")
-                return
-            curr_client = client
-
-        publish_topic = MQTT_OUT["out_topic"]
-        if not msg.topic.startswith("/") and \
-                not publish_topic.startswith("/"):
-            publish_topic = publish_topic + "/" + msg.topic
-        else:
-            publish_topic = publish_topic + msg.topic
-
-        logger.info(f"publish on {publish_topic} new message {json_text}")
-        curr_client.publish(publish_topic, payload=json.dumps(json_text))
+        MQTT_OUT["gateway"].gw_connect_device(device_id)
+        logging.info("Sending temperature value to " + device_id)
+        MQTT_OUT["gateway"].gw_send_telemetry(device_id, json_to_send)
+        MQTT_OUT["gateway"].gw_disconnect_device(device_id)
 
     except Exception as e:
         print(traceback.format_exc())
@@ -77,13 +69,18 @@ def on_connect_out(client, userdata, msg):
 
 
 def flatter_json(text):
-    #now = datetime.now().isoformat()
+    # now = datetime.now().isoformat()
     json_text = json.loads(text)
     if "m" in json_text:
         for elem in json_text["m"]:
             if "k" in elem and "v" in elem:
                 json_text[elem["k"]] = elem["v"]
         del json_text["m"]
+    if "meta" in json_text:
+        meta = json_text["meta"]
+        for k, v in meta.items():
+            json_text[k] = v
+        del json_text["meta"]
     return json_text
 
 
@@ -92,12 +89,12 @@ def on_disconnect_in(client, userdata, rc):
 
 
 def on_message_out(client, userdata, rc):
-    #logger.info("on_message_out MQTT BROKER")
+    # logger.info("on_message_out MQTT BROKER")
     pass
 
 
-def get_client(username: str = None, password: str = None, on_connect=None, on_message=None,on_disconnect=None):
-    client = mqtt.Client(transport='tcp', client_id='flatter'+str(time.time()))
+def get_client(username: str = None, password: str = None, on_connect=None, on_message=None, on_disconnect=None):
+    client = mqtt.Client(transport='tcp', client_id='flatter' + str(time.time()))
     client.on_connect = on_connect
     client.on_message = on_message
     client.on_disconnect = on_disconnect
@@ -114,6 +111,15 @@ def get_client(username: str = None, password: str = None, on_connect=None, on_m
     return client
 
 
+def send_cpu_memory_usage():
+    cpu_percent = psutil.cpu_percent()
+    memory_percent = psutil.virtual_memory().percent
+    data = {'cpu': cpu_percent, 'memory': memory_percent}
+    json_data = json.dumps(data)
+    logging.info("Sending gateway telemetries: " + json_data)
+    MQTT_OUT["gateway"].send_telemetry(data)
+
+
 if __name__ == '__main__':
     client_in = get_client(
         username=MQTT_IN["username"],
@@ -128,45 +134,24 @@ if __name__ == '__main__':
         60)
     MQTT_IN["client"] = client_in
 
-    # OUT Broker
-    if MQTT_OUT["host"] is not None and \
-        (MQTT_IN["host"] != MQTT_OUT["host"] or
-         MQTT_IN["port"] != MQTT_OUT["port"]):
-        client_out = get_client(
-            username=MQTT_OUT["username"],
-            password=MQTT_OUT["password"],
-            on_connect=on_connect_out_2,
-            on_disconnect=on_disconnect_in
-        )
-        client_out.connect(
-            MQTT_OUT["host"],
-            MQTT_OUT["port"],
-            60)
+    gateway = TBGatewayMqttClient(MQTT_OUT["host"], MQTT_OUT["port"], MQTT_OUT["username"], MQTT_OUT["password"])
+    MQTT_OUT["gateway"] = gateway
 
-        print("Going to connect to MQTT server Out ")
-        client_out.loop_start()
-        MQTT_OUT["client"] = client_out
-
-    else:
-        print("Going to use the same broker " + (MQTT_IN["host"] + ":"
-                                                 + str(MQTT_IN["port"])) + " for flattering output!")
+    gateway.connect()
     client_in.loop_start()
 
+    send_cpu_memory_usage()
     counter = 0
-    wait_time = 3
     while True:
-        time.sleep(wait_time)
+        time.sleep(30)
+        send_cpu_memory_usage()
         logger.info("wake up...")
-        if not MQTT_IN["client"].is_connected() \
-                or not MQTT_OUT["client"].is_connected():
+        if not MQTT_IN["client"].is_connected():
             if counter == 10:
                 logger.error(f"too long waiting.. going to exit after {wait_time} secs")
                 break
 
-            wait_time = wait_time + counter
             counter = counter + 1
             logger.warning(f"going to wait for reconnect... sleep for {wait_time} secs")
         else:
             counter = 0
-
-
